6f78d8249b61e2b1d495450f21a9d2c170a8a271
andrey.levchaev <lachesse@gmail.com>
2024-05-29 18:46:21 +0300

    fix(no-ref): import

--- src/main.ts
+++ src/main.ts
@@ -1,19 +1,18 @@
-import { ɵprovideZonelessChangeDetection } from '@angular/core';
-
-import { bootstrapApplication } from '@angular/platform-browser';
-import { AppComponent } from './app/app.component';
-import { provideEnvironmentNgxLoaderIndicator } from 'ngx-loader-indicator';
-import { provideAnimations } from '@angular/platform-browser/animations';
-import { HIGHLIGHT_OPTIONS } from 'ngx-highlightjs';
-import { provideRouter } from '@angular/router';
-import { GithubStarsService } from '@libraries/github/github-stars.service';
+import { provideZoneChangeDetection } from '@angular/core';
 import { provideHttpClient } from '@angular/common/http';
-import { DOMAIN } from '@libraries/token/token';
+import { bootstrapApplication } from '@angular/platform-browser';
+import { provideAnimations } from '@angular/platform-browser/animations';
+import { provideRouter } from '@angular/router';
 import { BaseHttpService } from '@libraries/base-http/base-http.service';
+import { GithubStarsService } from '@libraries/github/github-stars.service';
+import { DOMAIN } from '@libraries/token/token';
+import { HIGHLIGHT_OPTIONS } from 'ngx-highlightjs';
+import { provideEnvironmentNgxLoaderIndicator } from 'ngx-loader-indicator';
+import { AppComponent } from './app/app.component';
 
 bootstrapApplication(AppComponent, {
     providers: [
-        ɵprovideZonelessChangeDetection(),
+        provideZoneChangeDetection(),
         provideAnimations(),
         provideRouter([]),
         GithubStarsService,
